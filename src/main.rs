84f54905e0034a75fc8a73fa7ccaaed3f41d7bf1
alexandre <alexandre.dujardin1@outlook.com>
2025-03-21 17:16:54 +0100

    génération des jours avec noms et valeurs aléatoires

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use clap::{arg, Parser};
-struct Joueur {
+use rand::Rng;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    nom1: String,
+
+    #[arg(long)]
+    nom2: String,
+}
+
+struct Joueur {
     nom: String,
     vitalite: i32,
     vitesse: u64,
@@ -8,11 +19,8 @@ struct Joueur {
 }
 
 impl Joueur {
-
     fn new(new_name: &str, new_vitality: i32, new_speed: u64, new_strength: u32) -> Self { //Constructeur
-
         Joueur {
-
             nom: new_name.to_string(),
             vitalite: new_vitality,
             vitesse: new_speed,
@@ -21,7 +29,6 @@ impl Joueur {
     }
 
     fn afficher_stats (&self) {
-
         println!("name: {} ",self.nom);
         println!("vitalite: {} ",self.vitalite);
         println!("vitesse: {} ",self.vitesse);
@@ -29,9 +36,28 @@ impl Joueur {
     }
 }
 
-
 fn main() {
     println!("Hello, world!");
-    let j1 = Joueur::new("joueur 1",10,0,4);
-    j1.afficher_stats();
+
+    let args = Args::parse();
+
+    let mut rng = rand::thread_rng();
+
+    let joueur1 = Joueur::new(
+        &args.nom1,
+        rng.gen_range(50..=100),
+        rng.gen_range(30..=70),
+        rng.gen_range(20..=60),
+    );
+    let joueur2 = Joueur::new(
+        &args.nom2,
+        rng.gen_range(50..=100),
+        rng.gen_range(30..=70),
+        rng.gen_range(20..=60),
+    );
+
+    println!("--- Joueur 1 ---");
+    joueur1.afficher_stats();
+    println!("--- Joueur 2 ---");
+    joueur2.afficher_stats();
 }
